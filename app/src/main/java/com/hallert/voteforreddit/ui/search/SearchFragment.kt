0ccecf8e8894b7952abb2ffcaeab7a84957cca15
Thomas Haller <hallert60@gmail.com>
2021-01-17 19:50:38 -0600

    keyboard opens when opening SearchFragment

--- app/src/main/java/com/hallert/voteforreddit/ui/search/SearchFragment.kt
+++ app/src/main/java/com/hallert/voteforreddit/ui/search/SearchFragment.kt
@@ -1,11 +1,19 @@
 package com.hallert.voteforreddit.ui.search
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.activity.OnBackPressedCallback
 import com.hallert.voteforreddit.R
 import com.hallert.voteforreddit.ui.misc.bottomsheet.FullscreenBottomSheet
+import com.hallert.voteforreddit.util.KeyboardUtil
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : FullscreenBottomSheet() {
     override fun onCreateView(
@@ -14,6 +22,10 @@ class SearchFragment : FullscreenBottomSheet() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_search, container, false)
+        val text = root.findViewById<EditText>(R.id.search_edit_text)
+
+        dialog?.let { KeyboardUtil.openKeyboardDialog(it, text) }
+
         return root
     }
 }
---
+++ app/src/main/java/com/hallert/voteforreddit/util/KeyboardUtil.kt
@@ -0,0 +1,30 @@
+package com.hallert.voteforreddit.util
+
+import android.app.Dialog
+import android.content.Context
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+
+class KeyboardUtil {
+    companion object {
+        // used for opening the keyboard in fragments/activities
+        fun openKeyboard(context: Context, view: View) {
+            view.requestFocus()
+            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+        }
+
+        // used for opening the keyboard in dialogs
+        fun openKeyboardDialog(dialog: Dialog, view: View) {
+            view.requestFocus()
+            dialog.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        }
+
+        fun closeKeyboard(context: Context, view: View) {
+            view.requestFocus()
+            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+}
